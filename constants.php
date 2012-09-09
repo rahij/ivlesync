422b50ce5607187a4ca2e73595eaaeb2fff9f16e
rahij <rahij@geekmachine.(none)>
2012-09-10 07:36:24 +0800

    dummy constants.php added

---
+++ constants.php
@@ -0,0 +1,9 @@
+<?php
+$developerKey='google_api_developer_key';
+$clientId='google_api_client_id';
+$clientSecret='google_api_client_secret';
+?>
+
+<script type="text/javascript">
+var APIKey="your_ivle_lapi_key";
+</script>
