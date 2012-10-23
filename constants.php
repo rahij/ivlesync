ff59074d390cb26aa64039a311215f58561506e3
rahij <rahij.ramsharan@gmail.com>
2012-10-23 22:45:35 +0800

    removed constants file

--- constants.php
+++
@@ -1,9 +0,0 @@
-<?php
-$developerKey='AIzaSyDtRxIUq_D8Pp0usWQ9rzqZvKEYy02ubnY';
-$clientId='253590999479.apps.googleusercontent.com';
-$clientSecret='CVS5ZM83ji-d3a5fCMzMEBSP';
-?>
-
-<script type="text/javascript">
-var APIKey="ihGIAwinPHivcEbzy7XE2";
-</script>
